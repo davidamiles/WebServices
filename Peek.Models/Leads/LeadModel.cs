573e79b72a3cfecc704c972af9d32eae34f3e337
davidamiles <davidamiles@hotmail.com>
2018-11-06 20:34:12 -0700

    Leads models and repo

---
+++ Peek.Models/Leads/LeadModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Peek.Models.Leads
+{
+    public class LeadModel
+    {
+        public LeadModel()
+        {
+        }
+
+        public int Id { get; set; }
+        public string CustomerName { get; set; }
+        public string PhoneNum { get; set; }
+        public string ShipToAddress { get; set; }
+        public string City { get; set; }
+        public string Zipcode { get; set; }
+        public string Email { get; set; }
+        public string AssignedTo { get; set; }
+        public string LeadType { get; set; }
+        public string LeadStatus { get; set; }
+        public string LeadSource { get; set; }
+        public string LeadSubSource { get; set; }
+        public string LeadCreator { get; set; }
+        public DateTime AppointmentDate { get; set; }
+        public string Notes { get; set; }
+        public int ClientId { get; set; }
+        public DateTime CreateDate { get; set; }
+        public DateTime LastActivity { get; set; }
+        public DateTime JobCreationDate { get; set; }
+        public bool Active { get; set; }
+        public string Username { get; set; }
+        public string CoClient { get; set; }
+        public string LeadSubType { get; set; }
+    }
+}
---
+++ Peek.Repository/Leads/LeadsRepo.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Peek.Models.Leads;
+
+namespace Peek.Repository.Leads
+{
+    public class LeadsRepo : ILeadsRepo
+    {
+        private Entities.PeekViewEntities baseContext = null;
+
+        public LeadsRepo()
+        {
+            this.baseContext = new Entities.PeekViewEntities();
+        }
+
+        public void Delete(LeadModel item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Exists(LeadModel item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(LeadModel item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public LeadModel Select(LeadModel item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<LeadModel> Select()
+        {
+            IEnumerable<Entities.Lead> entities = this.baseContext.Leads.OrderByDescending(l => l.Id);
+
+            return (entities.Count() > 0) ? this.GetModelsFromEntities(entities) : null;
+        }
+
+        public IEnumerable<LeadModel> Select(int skip, int take)
+        {
+            IEnumerable<Entities.Lead> entities = this.baseContext.Leads.OrderByDescending(l => l.Id);
+
+            return (entities.Count() > 0) ? this.GetModelsFromEntities(entities) : null;
+        }
+
+        public void Update(LeadModel item)
+        {
+            throw new NotImplementedException();
+        }
+
+
+
+        
+        
+        
+        #region private
+
+        public List<LeadModel> GetModelsFromEntities(IEnumerable<Entities.Lead> entities)
+        {
+            List<LeadModel> models = new List<LeadModel>();
+
+
+
+
+
+            return models;
+        }
+
+        #endregion
+    }
+}
