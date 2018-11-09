b389eb2b8edab0f2b25c754db0635c1bfba11f40
davidamiles <davidamiles@hotmail.com>
2018-11-08 20:46:47 -0700

    DataContract definitions

--- Peek.Models/Leads/LeadModel.cs
+++ Peek.Models/Leads/LeadModel.cs
@@ -1,39 +1,98 @@
-﻿using System;
+﻿using Peek.Core.Utilities.Time;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Peek.Models.Leads
 {
+    [DataContract]
     public class LeadModel
     {
         public LeadModel()
         {
         }
 
+        [DataMember]
         public int Id { get; set; }
+
+        [DataMember]
         public string CustomerName { get; set; }
+
+        [DataMember]
         public string PhoneNum { get; set; }
+
+        [DataMember]
         public string ShipToAddress { get; set; }
+
+        [DataMember]
         public string City { get; set; }
+
+        [DataMember]
         public string Zipcode { get; set; }
+
+        [DataMember]
         public string Email { get; set; }
+
+        [DataMember]
         public string AssignedTo { get; set; }
+
+        [DataMember]
         public string LeadType { get; set; }
+
+        [DataMember]
         public string LeadStatus { get; set; }
+
+        [DataMember]
         public string LeadSource { get; set; }
+
+        [DataMember]
         public string LeadSubSource { get; set; }
+
+        [DataMember]
         public string LeadCreator { get; set; }
+
+        [IgnoreDataMember]
         public DateTime? AppointmentDate { get; set; }
+
+        [DataMember]
         public string Notes { get; set; }
+
+        [DataMember]
         public int? ClientId { get; set; }
+
+        [IgnoreDataMember]
         public DateTime? CreateDate { get; set; }
+
+        [IgnoreDataMember]
         public DateTime? LastActivity { get; set; }
+
+        [IgnoreDataMember]
         public DateTime? JobCreationDate { get; set; }
+
+        [DataMember(Name = "JobCreationDate")]
+        public string JobCreationDateString
+        {
+            get => DateTimeUtil.GetISODateTimeString(JobCreationDate);
+
+            set
+            {
+                CreateDate = DateTime.Parse(value);
+            }
+        }
+
+        [DataMember]
         public bool? Active { get; set; }
+
+        [DataMember]
         public string Username { get; set; }
+
+        [DataMember]
         public string CoClient { get; set; }
+
+        [DataMember]
         public string LeadSubType { get; set; }
     }
 }
--- Peek.WebService/WebServices/LeadsWebSvc.cs
+++ Peek.WebService/WebServices/LeadsWebSvc.cs
@@ -1,10 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity.Validation;
 using System.Linq;
 using System.Runtime.Serialization;
 using System.ServiceModel;
+using System.ServiceModel.Web;
 using System.Text;
 using Peek.Models.Leads;
+using Peek.Repository;
+using Peek.Repository.Leads;
 
 namespace Peek.WebService.WebServices
 {
@@ -18,7 +22,44 @@ namespace Peek.WebService.WebServices
 
         public IEnumerable<LeadModel> SelectLeads(string skip, string take)
         {
-            throw new NotImplementedException();
+            IEnumerable<LeadModel> models = null;
+
+            try
+            {
+                int s = int.Parse(skip);
+                int t = int.Parse(take);
+
+                ILeadsRepo repo = RepoFactory.GetLeadsRepo();
+                models = repo.Select(s, t);
+            }
+            catch (DbEntityValidationException ex)
+            {
+                OutgoingWebResponseContext response = WebOperationContext.Current.OutgoingResponse;
+                response.StatusCode = System.Net.HttpStatusCode.BadRequest;
+                response.StatusDescription = "Bad Request";
+
+                string errorMessage = base.GetErrorMessageFromEntityValidationException(ex);
+
+                base.LogMessage(errorMessage, Peek.Models.LogSeverity.Error, ex.StackTrace);
+            }
+            catch (FormatException ex)
+            {
+                OutgoingWebResponseContext response = WebOperationContext.Current.OutgoingResponse;
+                response.StatusCode = System.Net.HttpStatusCode.BadRequest;
+                response.StatusDescription = "Bad Request";
+
+                base.LogMessage(ex.Message, Peek.Models.LogSeverity.Error, ex.StackTrace);
+            }
+            catch (Exception ex)
+            {
+                OutgoingWebResponseContext response = WebOperationContext.Current.OutgoingResponse;
+                response.StatusCode = System.Net.HttpStatusCode.InternalServerError;
+                response.StatusDescription = "Internal Server Error";
+
+                base.LogMessage(ex.Message, Peek.Models.LogSeverity.Error, ex.StackTrace);
+            }
+
+            return models;
         }
     }
 }
